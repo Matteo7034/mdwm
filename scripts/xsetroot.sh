3ef7560e3ca15353355af511394d4b8c6d3243c8
Matteo7034 <matteo7034@gmail.com>
2026-08-06 12:07:10 +0200

    changed borders and added to xsetroot script battery watt usage

--- scripts/xsetroot.sh
+++ scripts/xsetroot.sh
@@ -1,6 +1,15 @@
 
 while true; do
     BAT=`cat /sys/class/power_supply/BAT0/capacity`
-    xsetroot -name " $(date +%H:%M) | $(date +%d/%m/%y) | 🔋 $BAT"
+    if [ -f /sys/class/power_supply/BAT0/current_now ] && [ -f /sys/class/power_supply/BAT0/voltage_now ]; then
+        CURRENT=$(cat /sys/class/power_supply/BAT0/current_now)
+        VOLTAGE=$(cat /sys/class/power_supply/BAT0/voltage_now)
+        WATTS=$(awk "BEGIN {printf \"%.2fW\", ($CURRENT * $VOLTAGE)/1000000000000}")
+    else
+        WATTS="N/A"
+    fi
+
+
+    xsetroot -name " $(date +%H:%M) | $(date +%a) | $(date +%d/%m/%y) | 🔋 $BAT% ($WATTS) "
 	sleep 60
 done
